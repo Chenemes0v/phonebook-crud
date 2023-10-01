0873cbdfd2227b78977899e289316d8c9905d0f7
Chenemes0v <99468374+Chenemes0v@users.noreply.github.com>
2023-10-01 16:37:05 +0600

    Initial commit

---
+++ src/test/java/com/chenemesov/phonebook/PhonebookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chenemesov.phonebook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhonebookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
